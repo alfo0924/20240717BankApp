ce099e0ac933994005a0bc5608aedac90e8d89ea
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-07-22 10:16:33 +0800

    v1.29 commit n push

--- app/src/main/java/com/example/a20240717bankapp/JapaneseCurrencyExchange.java
+++ app/src/main/java/com/example/a20240717bankapp/JapaneseCurrencyExchange.java
@@ -3,9 +3,6 @@ package com.example.a20240717bankapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
